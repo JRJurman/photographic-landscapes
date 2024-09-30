71d7da591e7c53df61bc7dcc71ade8f3f92e540d
Jesse Jurman <j.r.jurman@gmail.com>
2024-09-29 22:39:27 -0400

    page implementations, empty 404

--- server.js
+++ server.js
@@ -18,24 +18,71 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', '.hbs');
 app.set('views', './pages');
 
+const homePageEntry = '1g3BTLM0CkE2P4wAidOlDD';
+const showsPageEntry = '12iAUWL842bJzzgtauHhd3';
+const photosPageEntry = '6GPzfSeoHP2oXNz7NAC6dF';
+const contactPageEntry = '5243Thx57mMBoG4ZlS98pA';
+
+const processEventData = (event) => {
+	return {
+		title: event.fields.title,
+		photo: {
+			file: event.fields.photo.fields.file.url,
+			description: event.fields.photo.fields.description,
+		},
+		startDate: event.fields.startDate,
+		endDate: event.fields.endDate,
+		description: documentToHtmlString(event.fields.description),
+	};
+};
+
 const test = async () => {
-	const homePageEntry = '1g3BTLM0CkE2P4wAidOlDD';
+	const showsPage = await client.getEntry(showsPageEntry);
+	const { events } = showsPage.fields;
+	console.log(processEventData(events.at(0)).description);
+
 	const homePage = await client.getEntry(homePageEntry);
 	const { blurb } = homePage.fields;
-	console.log(blurb);
-	console.log();
+	console.log(documentToHtmlString(blurb));
 };
 
 // test();
 
-app.get('/:page', async (req, res) => {
-	const homePageEntry = '1g3BTLM0CkE2P4wAidOlDD';
+app.get('/', async (req, res) => {
+	req.params.page = 'home';
 	const homePage = await client.getEntry(homePageEntry);
 	const { graphic, blurb } = homePage.fields;
 
-	res.render(req.params.page, { graphic: graphic.fields, blurb: documentToHtmlString(blurb) });
+	res.render('home', { graphic: graphic.fields, blurb: documentToHtmlString(blurb) });
+});
+
+app.get('/shows', async (req, res) => {
+	const showsPage = await client.getEntry(showsPageEntry);
+	const { events } = showsPage.fields;
+
+	// TODO filter and sort
+	const eventsData = events.map(processEventData);
+	res.render('shows', { events: eventsData });
+});
+
+app.get('/photos', async (req, res) => {
+	const photosPage = await client.getEntry(photosPageEntry);
+	const { thumbnails } = photosPage.fields;
+
+	res.render('photos', { thumbnails });
+});
+
+app.get('/contact', async (req, res) => {
+	const contactPage = await client.getEntry(contactPageEntry);
+	const { graphic, blurb } = contactPage.fields;
+
+	res.render('contact', { graphic: graphic.fields, blurb: documentToHtmlString(blurb) });
+});
+
+app.get('*', async (req, res) => {
+	res.render('404', {});
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port http://localhost:${port}`);
 });
